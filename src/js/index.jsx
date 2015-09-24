48053e38c83610d2ed0ae68c83486c9ee180aeb5
= <kristoffer@kristofferhebert.com>
2015-09-23 21:54:43 -0700

    updated index with imports from components

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,7 +1,12 @@
+import Input from 'components/Input'
+import Form from 'components/Form'
+import Login from 'components/Login'
+import Regist from 'components/Register'
+
 let ReactForm = {
-    Input : Input,
-    Form : Form,
-    Login : Login,
+    Input: Input,
+    Form: Form,
+    Login: Login,
     Register: Register
 }
 
